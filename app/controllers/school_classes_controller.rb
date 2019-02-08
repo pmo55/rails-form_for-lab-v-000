adf2e2ef9b43cd95787ce0b7fd8b192c91f2138a
Parsa Moshiri <parsamoshiri5@gmail.com>
2019-02-08 18:03:15 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,6 +1,13 @@
 class SchoolClassesController < ApplicationController
   def show
-    @school_classes
+    @school_classes = SchoolClass.find(params[:id])
+  end
   
+  def new
+    @school_class = SchoolClass.new
+  end
   
+  def create
+    
+  end
 end
