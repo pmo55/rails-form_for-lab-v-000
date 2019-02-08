8d3f6f8d1f6a3811bcd7e7f457c8c164a9ff3dc9
Parsa Moshiri <parsamoshiri5@gmail.com>
2019-02-08 18:06:27 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,20 @@
 class StudentsController < ApplicationController
   
+  def show
+    @student = Student.find(params[:id])
+  end
+  
+  def new
+    @student = Student.new
+  end
+  
+  def edit
+    @student = Student.find(params[:id])
+  end
+  
+  def create
+    
+  end
   
   
 end
